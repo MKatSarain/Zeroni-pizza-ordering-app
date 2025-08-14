e3cbcdfa75cead22a30fce60fe0f2bf3dc57faf6
PURE Cupcakes Desktop 2 <purecupcakes-desktop2@PUREs-iMac.local>
2025-08-13 20:26:41 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, redirect, render_template, session
+import configparser
+
+app = Flask(__name__)
+app.secret_key = "your_secret_key"  # Make this more secure in production!
+
+# FUNCTIONS
+
+
+# ROUTES
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/menu')
+def menu():
+    return render_template('menu.html')
+
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
+
+@app.route('/order')
+def order():
+    if 'cart' not in session:
+        session['cart'] = []
+    return render_template('order.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5001)
+    
